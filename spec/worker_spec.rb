660abecce92ceb12c9be3b32cbf5c0342a3dd73a
Trevor John <trevor@john.tj>
2019-12-23 22:05:58 -0500

    Worker has a position parses instruction to movement

---
+++ spec/worker_spec.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Worker, "#move" do
+  context "when invalid instruction passed" do
+    it "does not move" do
+      expect(subject.move("bad")).to eq "0,0"
+    end
+  end
+
+  context "moving left" do
+    it "decreases x coordinate" do
+      expect(subject.move(described_class::LEFT)).to eq "-1,0"
+    end
+  end
+
+  context "moving right" do
+    it "increases x coordinate" do
+      expect(subject.move(described_class::RIGHT)).to eq "1,0"
+    end
+  end
+
+  context "moving up" do
+    it "increases y coordinate" do
+      expect(subject.move(described_class::UP)).to eq "0,1"
+    end
+  end
+
+  context "moving down" do
+    it "decreases y coordinate" do
+      expect(subject.move(described_class::DOWN)).to eq "0,-1"
+    end
+  end
+
+  it "keeps track of position" do
+    expect(subject.move(described_class::DOWN)).to eq "0,-1"
+    expect(subject.move(described_class::DOWN)).to eq "0,-2"
+    expect(subject.move(described_class::RIGHT)).to eq "1,-2"
+    expect(subject.move(described_class::RIGHT)).to eq "2,-2"
+    expect(subject.move(described_class::UP)).to eq "2,-1"
+    expect(subject.move(described_class::LEFT)).to eq "1,-1"
+    expect(subject.move(described_class::UP)).to eq "1,0"
+    expect(subject.move(described_class::LEFT)).to eq "0,0"
+  end
+end
---
+++ worker.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+class Worker
+  UP    = "^".freeze
+  DOWN  = "v".freeze
+  LEFT  = "<".freeze
+  RIGHT = ">".freeze
+
+  def initialize
+    @x = 0
+    @y = 0
+  end
+
+  def move(direction)
+    update_position(direction)
+    "#{x},#{y}"
+  end
+
+  private
+
+  attr_reader :x, :y
+
+  def update_position(direction)
+    case direction
+    when UP
+      @y += 1
+    when DOWN
+      @y -= 1
+    when RIGHT
+      @x += 1
+    when LEFT
+      @x -= 1
+    end
+  end
+end
